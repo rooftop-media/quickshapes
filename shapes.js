a7759e42b919105d907a75ce4fe56a6d4bffb13f
Ben Holland <bhollan5@kent.edu>
2021-01-29 00:49:35 -0500

    Adding docs

--- shapes.js
+++ shapes.js
@@ -1,58 +1,51 @@
 
 
-/**  Subfunction to draw characters.      */
-function draw_if( condition ) {
-    if (condition) {
-	process.stdout.write("#");
-    }
-    else {
-	process.stdout.write(" ");
-    }
+
+/**  A graph function!   */
+function graph( w, h, rule ) {
+
+    /**  Build the string here.                                  */
+    var shape_string  = "";
+
+    /**  Loop through lines, starting at height, going down.     */
+    for ( var y = h; y > 0; y-- ) {
 	
-}
-
-
-/**  Drawing a box.                       */
-function draw_box( height, width, filled ) {
-
-    for ( var y = 0; y < height; y++ ) {
-	for ( var x = 0; x < width; x++ ) {
-	    var is_edge = ( y == 0 || x == 0 || y == (height-1) || x == (width-1) );
-	    draw_if( filled || is_edge );
-	}
+	/**  For each line, go through each "x" from 0 to width.       */
+	for ( var x = 0; x < w; x++ ) {
+	    
+	    /**  Draw if the rule is true when passed x and y.               */
+	    if ( rule(x,y) )
+		process.stdout.write("*");
+	    else 
+		process.stdout.write(" ");
+	    
+	} //  End of 1 line.
+	
+	/**  Write an endline. */
 	process.stdout.write("\n");
+	
     }
-    process.stdout.write("\n");
 
 }
 
-draw_box( 4, 6, true );
-draw_box( 5, 7, false );
 
-return;
 
-function draw_ellipse( height, width, filled ) {
-    for ( var y = 0; y < height; y++ ) {
-	for ( var x = 0; x < width; x++ ) {
-	    draw_if( Math.pow(x+1,2) + Math.pow(y+1,2) == Math.pow(width/2,2) );
-	}
-	process.stdout.write("\n");
-    }
-    process.stdout.write("\n");
+function draw_line(x, y) {
+    return y == Math.floor( (x * x) / 6 );
+}
+graph( 20, 20, draw_line );
+
+
+function draw_ellipse( x, y ) { 
+
+    /**  The radius of our circle (6) squared */
+    var radius_sq  = 36;
+    
+    /**  How far the point is from the center. */
+    var distance   = Math.pow(x-10, 2) + Math.pow(y-10, 2);
+    var diff       = Math.abs(radius_sq - distance);
+    return ( diff < 8 );
 }
 
-draw_ellipse( 8, 8, true );
+graph( 20, 20, draw_ellipse );
 
-
-/*
-
-  ____
-  ____
-  ____
-  ____
-
-
-
-
-
- */
