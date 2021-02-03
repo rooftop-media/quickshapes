5a0aceb8e2edf17410b4ff42b5bbaf21d932e924
Ben Holland <bhollan5@kent.edu>
2021-02-03 01:46:23 -0500

    restructure

---
+++ versions/2.0/shapes.js
@@ -0,0 +1,21 @@
+//  quickshapes / v2.0 / shapes.js 
+//  Version 2.0 of the QuickShapes library.
+
+
+
+//  Okay let's make some shapes, then.  
+module.exports = class QuickShape {
+
+    constructor() {
+
+	this.height = 0;
+
+	this.width  = 0;
+
+	this.type   = "square";
+
+    }
+    
+
+
+}
---
+++ versions/2.0/test.js
@@ -0,0 +1,12 @@
+//  shapes / v2.0 / test.js
+//  Test the functions in QuickShape v2.0.  
+
+var QuickShape = require("./shapes.js");
+
+
+console.log("\n Testing QuickShapes v 2.0.\n\n" );
+
+//  Make a new quickshape:
+
+var my_shape = new QuickShape();
+
